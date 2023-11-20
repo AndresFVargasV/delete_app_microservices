5de5b58856f8d93416a2744265803ef8c31a55a7
AndresFVargasV <afvasquez@uninorte.edu.co>
2023-11-20 18:24:30 -0500

    comment added

--- source/main.py
+++ source/main.py
@@ -3,14 +3,18 @@ import dbconfig as dbase
 from flask_cors import CORS
 
 app = Flask(__name__)
+
+# Configuración para permitir peticiones desde cualquier origen
 CORS(app)
 
+# Ruta base de la API
 @app.route("/api", methods=["GET"])
 def api():
     return jsonify({'msg': 'Hello World'})
 
-@app.route("/api/delete", methods=["DELETE"])
-def eliminar_usuario():
+# Ruta para eliminar un usuario
+@app.route("/api/delete/<numero_documento>", methods=["DELETE"])
+def eliminar_usuario(numero_documento):
     try:
         client = dbase.establecer_conexion()
 
@@ -25,25 +29,22 @@ def eliminar_usuario():
         if db is None or collection is None:
             raise Exception("No se pudo establecer la conexión con MongoDB")
 
-        
-        datos = request.args.get('numero_documento')
-        # Verificar si se proporcionaron datos
-        if not datos:
-            raise Exception("Número de documento no proporcionado en la solicitud")
-
         # Eliminar el usuario
-        if (check_user(datos) is None):
+        if (check_user(numero_documento) is None):
             raise Exception("El usuario no existe")
         else:
-            collection.delete_one({'numero_documento': datos})
+            collection.delete_one({'numero_documento': numero_documento})
 
-
+        # Respondemos al cliente con un mensaje de éxito
         return jsonify({'mensaje': 'Usuario eliminado correctamente'})
     except Exception as e:
+
+        # Retornar un error
         return make_response(jsonify({'error': str(e)}), 500)
     finally:
         dbase.cerrar_conexion(client)
 
+# Función para verificar si un usuario existe
 def check_user(datos):
     try:
         client = dbase.establecer_conexion()
@@ -66,13 +67,15 @@ def check_user(datos):
         # Buscar el usuario
         user = collection.find_one({'numero_documento': datos})
 
+        # Retornar el usuario encontrado
         return user
     except Exception as e:
+        # Retornar un error
         return make_response(jsonify({'error': str(e)}), 500)
     finally:
         if client:
             client.close()
 
-    
+# Iniciamos la aplicación
 if __name__ == "__main__":
     app.run(debug=True)
